4bb74e9b15ab4b970bb1a76401def8599ae91fd2
kukueb <138104770+kukueb@users.noreply.github.com>
2023-07-26 08:47:27 +0300

    updated
    + Extends base.html
    + "block" changes

--- advertisements/app_advertisements/urls.py
+++ advertisements/app_advertisements/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import index
+from .views import index, top_sellers, advertisements, advertisement_post, login, profile, register
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisements/', advertisements, name='advertisements'),
+    path('advertisements_post/', advertisement_post, name='advertisement-post'),
+    path('login/', login, name='login'),
+    path('profile/', profile, name='profile'),
+    path('register/', register, name='register'),
 ]
