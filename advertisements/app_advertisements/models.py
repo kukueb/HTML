b917f0eb04544c8066cb89901ce849ef89069486
kukueb <138104770+kukueb@users.noreply.github.com>
2023-08-03 10:18:55 +0300

    updated again
    changed:
    1 - table name is Advertisement
    2 - after quaryset log we get styled info
    как говорил классик: "Вот так вот"

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField("заголовок", max_length=128)
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("торг", help_text="отметьте, если торг уместен")
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "Advertisement"
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title='{self.title}', price={self.price:.2f})"
